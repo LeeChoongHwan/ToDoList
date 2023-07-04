33cf6f3bc13d8b9ee7d59157bb7aaf580b98d706
이동규 <dklee1619@naver.com>
2023-07-04 20:33:28 +0900

    memo delete

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,37 @@ class SubPage extends StatelessWidget {
         backgroundColor: Colors.orange,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("정말로 삭제하시겠습니까?"),
+                    actions: [
+                      // 취소 버튼
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("취소"),
+                      ),
+                      // 확인 버튼
+                      TextButton(
+                        onPressed: () {
+                          memoList.removeAt(index); // index에 해당하는 항목 삭제
+                          Navigator.pop(context); // 팝업 닫기
+                          Navigator.pop(context); // HomePage 로 가기
+                        },
+                        child: Text(
+                          "확인",
+                          style: TextStyle(color: Colors.pink),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
             icon: Icon(
               Icons.delete,
               color: Colors.white,
