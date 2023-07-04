b7cb8291255d994cf572d6a6335151df6988e407
LeeChoongHwan <choo901@naver.com>
2023-07-04 16:43:25 +0900

    make frame

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,74 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          title: Text(
-            "Todo",
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
+      home: MainPage(),
+    );
+  }
+}
+
+// 홈 페이지
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+        title: Text(
+          "Todo",
+          style: TextStyle(color: Colors.white),
         ),
-      body: Text(""),
       ),
+      body: Text(""),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => SubPage()),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class SubPage extends StatelessWidget {
+  SubPage({super.key});
+
+  TextEditingController contentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: Text(""),
     );
   }
 }
