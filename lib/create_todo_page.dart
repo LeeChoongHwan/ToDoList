94235731430053ce73facd5c34e7c21cb8f50ab8
LeeChoongHwan <choo901@naver.com>
2023-07-06 20:58:43 +0900

    add title

--- lib/create_todo_page.dart
+++ lib/create_todo_page.dart
@@ -10,15 +10,18 @@ class CreateTodoPage extends StatelessWidget {
   final int index;
   final bool isModify;
 
+  TextEditingController titleController = TextEditingController();
   TextEditingController contentController = TextEditingController();
 
-  late String textValue;
+  String contentValue = "";
+  String titleValue = "";
 
   @override
   Widget build(BuildContext context) {
     MemoService memoService = context.read<MemoService>();
     Memo memo = memoService.memoList[index];
 
+    titleController.text = memo.title;
     contentController.text = memo.content;
 
     return Scaffold(
@@ -27,8 +30,11 @@ class CreateTodoPage extends StatelessWidget {
         actions: [
           TextButton(
               onPressed: () {
-                memoService.updateMemo(index: index, content: textValue);
-                Navigator.pop(context);
+                if (contentValue.isNotEmpty && titleValue.isNotEmpty) {
+                  memoService.updateMemo(
+                      index: index, content: contentValue, title: titleValue);
+                  Navigator.pop(context);
+                }
               },
               child: Text(
                 isModify ? "수정" : "저장",
@@ -38,20 +44,42 @@ class CreateTodoPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(32),
-        child: TextField(
-            controller: contentController,
-            decoration: const InputDecoration(
-                hintText: "내용을 입력하세요",
-                border: InputBorder.none,
-                filled: true,
-                fillColor: AppColors.textFieldColor),
-            autofocus: true,
-            maxLines: null,
-            expands: true,
-            keyboardType: TextInputType.multiline,
-            onChanged: (value) {
-              textValue = value;
-            }),
+        child: Column(
+          children: [
+            TextField(
+                controller: titleController,
+                decoration: const InputDecoration(
+                    hintText: "제목을 입력하세요",
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor: AppColors.textFieldColor),
+                autofocus: true,
+                maxLines: 1,
+                expands: false,
+                keyboardType: TextInputType.multiline,
+                onChanged: (value) {
+                  titleValue = value;
+                }),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 300,
+              child: TextField(
+                  controller: contentController,
+                  decoration: const InputDecoration(
+                      hintText: "내용을 입력하세요",
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: AppColors.textFieldColor),
+                  autofocus: true,
+                  maxLines: null,
+                  expands: true,
+                  keyboardType: TextInputType.multiline,
+                  onChanged: (value) {
+                    contentValue = value;
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/memo_service.dart
+++ lib/memo_service.dart
@@ -3,21 +3,20 @@ import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Memo {
-  Memo({required this.content, this.isChecked = false});
+  Memo({required this.title, this.isChecked = false, required this.content});
 
-  String content;
+  String title;
   bool isChecked;
+  String content;
 
   Map<String, dynamic> toJson() {
-    return {
-      'content': content,
-      'isPinned': isChecked,
-    };
+    return {'title': title, 'isPinned': isChecked, 'content': content};
   }
 
   Memo.fromJson(Map<String, dynamic> json)
-      : content = json['content'],
-        isChecked = json['isPinned'] ?? false;
+      : title = json['title'],
+        isChecked = json['isPinned'] ?? false,
+        content = json['content'];
 }
 
 class MemoService with ChangeNotifier {
@@ -27,15 +26,20 @@ class MemoService with ChangeNotifier {
 
   List<Memo> memoList = [];
 
-  Future<void> createMemo({required String content}) async {
-    Memo memo = Memo(content: content);
+  Future<void> createMemo(
+      {required String content, required String title}) async {
+    Memo memo = Memo(title: title, content: content);
     memoList.add(memo);
     notifyListeners();
     await saveMemoList();
   }
 
-  Future<void> updateMemo({required int index, required String content}) async {
+  Future<void> updateMemo(
+      {required int index,
+      required String content,
+      required String title}) async {
     Memo memo = memoList[index];
+    memo.title = title;
     memo.content = content;
     notifyListeners();
     await saveMemoList();
@@ -67,10 +71,10 @@ class MemoService with ChangeNotifier {
   updatePinMemo({required int index}) {
     Memo memo = memoList[index];
     memo.isChecked = !memo.isChecked;
-    memoList = [
-      // ...memoList.where((element) => !element.isChecked),
-      // ...memoList.where((element) => element.isChecked)
-    ];
+    // memoList = [
+    //   ...memoList.where((element) => !element.isChecked),
+    //   ...memoList.where((element) => element.isChecked)
+    // ];
     notifyListeners();
     saveMemoList();
   }
